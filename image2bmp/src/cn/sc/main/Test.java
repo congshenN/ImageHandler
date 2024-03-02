c9c8883cd5226d6f7ca40251117787d46e200bee
congshen <54343372+congshenN@users.noreply.github.com>
2024-03-02 14:14:21 +0800

    Update Test.java

--- image2bmp/src/cn/sc/main/Test.java
+++ image2bmp/src/cn/sc/main/Test.java
@@ -1,1 +1,16 @@
+package cn.sc.main;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.text.Utilities;
+
+import cn.sc.util.ImageUtil;
+
+public class Test {
+	public static void main(String[] args) throws Exception {
+		ImageIO.write(ImageUtil.toBMP(ImageUtil.setSize(ImageIO.read(new File("D:\\文档资料\\myjar\\compurion2.bmp")), 720, 576)), "bmp", new File("D:\\文档资料\\myjar\\compurion3.bmp"));
+	}
+}
 
