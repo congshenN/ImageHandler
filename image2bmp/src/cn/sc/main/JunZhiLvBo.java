ccac3c05cf562707c215f2d481a6cde0db0da869
congshen <54343372+congshenN@users.noreply.github.com>
2024-03-02 14:09:54 +0800

    Update JunZhiLvBo.java

--- image2bmp/src/cn/sc/main/JunZhiLvBo.java
+++ image2bmp/src/cn/sc/main/JunZhiLvBo.java
@@ -1,1 +1,18 @@
-ss
+package cn.sc.main;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+import cn.sc.util.ImageUtil;
+
+public class JunZhiLvBo {
+	public static void main(String[] args) throws Exception {
+		
+		BufferedImage read = ImageUtil.JunZhiLvBo(ImageIO.read(new File("D:\\文档资料\\myjar\\DORANI_Test1.bmp")));
+		ImageIO.write(read, "bmp", new File("D:\\文档资料\\myjar\\DORANI4.bmp"));
+		
+		
+	}
+}
